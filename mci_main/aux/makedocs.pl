d67450337192595aef178aaf2b4de9057a8375b1
mjb8086 <91055630+mjb8086@users.noreply.github.com>
2022-10-29 19:44:25 +0100

    more names

--- mci_main/aux/makedocs.pl
+++ mci_main/aux/makedocs.pl
@@ -3,25 +3,26 @@ use v5.32;
 use warnings;
 
 my @FORENAMES_MALE = qw(
-Andrew Aaron Arnold Ainsley Antony Alexander Alf
-Bryan Bryson Bernard Bruce Ben Barry
-Christopher Carl Cyril Ciaran
+Andrew Aaron Arnold Ainsley Antony Alexander 
+Alf Archer Alfred
+Bryan Bryson Bernard Bruce Ben Barry Bill
+Christopher Carl Cyril Ciaran Colin
 David Daniel Damon
-Edward Edgar Ethel
+Edward Edgar Ethel Ebenezer
 Felix Fontaine
 George Glenn Godwin Graham Gerald
 Harold Hubert Hugh Horace
 Ian
 Joseph James Jonathan
 Kenneth
-Lawrence Leslie Llaim
+Lawrence Leslie Llaim Louis Linus
 Michael Mark Matthew Maximillian Morris
 Nicholas Neville
-Paul Peter Philip
+Paul Peter Philip Patrick
 Quentin
-Robert Ryan Roger Rowan
+Robert Ryan Roger Rowan Robin
 Simon Stanley Samuel Stephen
-Terrance
+Terrance Tom
 Vincent
 William Winston
 );
@@ -31,7 +32,7 @@ Amy Annette Anita Alice Allison
 Bernadette Beatrice
 Carolyn Christine Ciara Claire Catherine Carrie Charlene Chloe
 Doreen Danielle Deborah
-Ellenor Emma Evangeline
+Ellenor Emma Evangeline Elizabeth
 Felicity
 Georgina Gail Gillian
 Hannah Heather
@@ -51,18 +52,22 @@ Yvonne
 my @SURNAMES = qw(
 Anderson Armstrong Alburn Alder Ashen
 Bates Brown Bolton Boyce Boyd Blair
-Connolly Columbus Caroll Chamberlain Claypool Coxon Currie
-Dobbin Dell Delvin
+Connolly Columbus Caroll Chamberlain Claypool Coxon Currie Carruthers
+Crossett Crooks Cutter Cage Cooke Chapman
+Dobbin Dell Delvin Davis Doyle Defoe
 Erskin
+Forsythe
 Griffin Goodman Green
 Hyde Hughes Hamilton
-Laverty
+Kane
+Laverty Lumberg Livingstone Livesay Locke
 Mitchell Moorehead Moore Montgomery McMullan McCallion McCullogh Morrison McGlone
 Norris Newton
 Oldman Overend O'Leary
 Parke Pollard Paulson
 Ramsey Rochefort Robinson
-Smith Stuart Steele Smyth
+Smith Stuart Steele Smyth Stone Stallman
+Theroux
 Underwood
 Wall Whiteside Whitehead Willowfield Woods
 );
