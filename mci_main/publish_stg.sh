4996fc3342095b68e43c63765e8d0bfe88cb9ef0
mjb8086 <91055630+mjb8086@users.noreply.github.com>
2022-11-05 13:51:42 +0000

    add barlow and cabin fonts

--- mci_main/publish_stg.sh
+++ mci_main/publish_stg.sh
@@ -9,6 +9,9 @@ echo "1. BUILDING RELEASE CONFIGURATION >>>>>"
 dotnet publish --configuration Release
 echo "2. STOPPING DAEMON ON REMOTE SERVER >>>>>>"
 ssh $HOST systemctl stop mci-platform
+
+# Todo: Copy DB if does not exist. Set DB permissions.
+
 echo "3. RSYNC IT >>>>>"
 rsync -prvc --delete -e "ssh -p $SSH_PORT" "$PUB" $HOST:/var/www/mci-platform
 echo "4. START IT >>>>"
